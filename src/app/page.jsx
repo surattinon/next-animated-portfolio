92358de3f088a4cde2235df281cd7058a2e072d4
surattinon <surattinon.basu@gmail.com>
2024-04-25 11:07:07 +0700

    remove skills section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Homepage = () => {
-  const ref = useRef();
-
-  const { scrollYProgress } = useScroll({ target: ref });
-  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-75%"]);
-
-  const skills = [
-    {
-      id: "1",
-      title: "Linux skill",
-      desc: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-      textColor: "text-[#ffffff]",
-    },
-    {
-      id: "2",
-      title: "Developer skill",
-      desc: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-      textColor: "text-[#ffffff]",
-    },
-    {
-      id: "3",
-      title: "Network skill",
-      desc: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-      textColor: "text-[#ffffff]",
-    },
-  ];
-
   return (
     <motion.div
       className="h-full"
@@ -77,63 +51,18 @@ const Homepage = () => {
             cupidatat.
           </p>
           <div className="flex gap-4 font-semibold">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-white text-black">
-              View my work
-            </button>
-            <button className="p-4 rounded-lg ring-1 ring-white">
-              Contact me
-            </button>
+            <Link href="/portfolio">
+              <button className="p-4 rounded-lg ring-1 ring-black bg-white text-black">
+                View my work
+              </button>
+            </Link>
+            <Link href="/contact">
+              <button className="p-4 rounded-lg ring-1 ring-white">
+                Contact me
+              </button>
+            </Link>
           </div>
         </motion.div>
-      </div>
-      <div
-        className="h-[300vh] bg-gradient-to-b from-[#000000] to-[#111111] w-screen"
-        ref={ref}
-      >
-        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
-          <motion.div className="flex" style={{ x }}>
-            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-t from-[#000000] to-[#111111]">
-              <div className="h-screen w-full flex flex-col items-center justify-center">
-                <div className="h-60">
-                  <div className="flex items-center justify-start">
-                    <div className="fixed w-[30px] h-[30px] bg-white blur-xl" />
-                    <div className="fixed w-[30px] h-[30px] rounded-full bg-white m-auto" />
-                    <div className="fixed w-[50vw] h-[5px] bg-white translate-x-4" />
-                  </div>
-                </div>
-                <div className="flex flex-col items-center justify-center gap-8 mt-20">
-                  <h1 className="fixed text-8xl h-1/2 justify-start font-bold pt-20">
-                    Skills
-                  </h1>
-                </div>
-              </div>
-            </div>
-            {skills.map((item) => (
-              <div
-                className="h-screen w-screen flex items-center justify-center bg-gradient-to-t from-[#000000] to-[#111111]"
-                key={item.id}
-              >
-                <div className="h-screen w-full flex flex-col items-center justify-center">
-                  <div className="h-60">
-                    <div className="flex items-center justify-center">
-                      <div className="fixed w-[30px] h-[30px] bg-white blur-xl" />
-                      <div className="fixed w-[30px] h-[30px] rounded-full bg-white m-auto" />
-                      <div className="fixed w-screen h-[5px] bg-white translate-x-4" />
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-center justify-center gap-8 mt-20">
-                    <h1 className="fixed text-4xl h-1/2 justify-start font-semibold">
-                      {item.title}
-                    </h1>
-                    <p className="fixed w-screen px-12 pb-32 text-center">
-                      {item.desc}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </motion.div>
-        </div>
       </div>
     </motion.div>
   );
